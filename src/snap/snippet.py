72359ee341577c5b9cc94e91aae124a842aa7ab3
CEO1 <christian4onos@outlook.com>
2024-09-07 16:33:48 +0100

    create new snippet

---
+++ src/snap/snippet.py
@@ -0,0 +1,15 @@
+class Snippet:
+    def __init__(self, name: str, category: str):
+        self.name: str = name
+        self.category: str = category
+        self.content: str | None = None
+
+    def __str__(self):
+        print(f"new class called {self.__class__.__name__} has been created")
+        return f"{self.name} ({self.category})"
+
+    def set_content(self, content: str) -> None:
+        self.content = content
+
+
+
